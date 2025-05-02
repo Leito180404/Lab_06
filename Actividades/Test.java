631f73273237907b68705d40915b54bf5913590e
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-05-02 08:37:43 -0500

    Update Test.java

---
+++ Actividades/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+   public static void main(String[] args) {
+        StackArray<Integer> stackInt = new StackArray<>(4);
+        System.out.println("Esta vacia? " + stackInt.isEmpty());
+   } 
+}
