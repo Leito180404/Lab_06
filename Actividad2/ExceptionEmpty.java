55b094e870c85fc855d7dcd7a5b9333e1e06aab3
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-05-05 08:02:20 -0500

    Create ExceptionEmpty.java

---
+++ Actividad2/ExceptionEmpty.java
@@ -0,0 +1,13 @@
+package Actividad2;
+
+public class ExceptionEmpty extends Exception {
+
+    public ExceptionEmpty() {
+        super("La pila no tiene nada.");
+    }
+
+    public ExceptionEmpty(String message) {
+        super(message);
+    }
+}
+
