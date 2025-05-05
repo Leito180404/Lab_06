b608ce48e1f1dd7cd52f88080042f80721f62ffb
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-05-05 08:34:45 -0500

    Update Test.java

--- Actividad2/Test.java
+++ Actividad2/Test.java
@@ -4,28 +4,28 @@ public class Test {
    public static void main(String[] args) {
         QueueLink<Integer> queueInt = new QueueLink<>();
 
-        System.out.println("¿Está vacía? " + queueInt.isEmpty());
+        System.out.println("¿Está vacia? " + queueInt.isEmpty());
 
         try {
             queueInt.enqueue(10);
             queueInt.enqueue(20);
             queueInt.enqueue(30);
-            System.out.println("Cola después de encolar 10, 20, 30: " + queueInt.toString()); 
+            System.out.println("Cola despues de encolar 10, 20, 30: " + queueInt.toString()); 
 
-            System.out.println("Front: " + queueInt.front());
+            System.out.println("Frente: " + queueInt.front());
 
-            System.out.println("Back: " + queueInt.back()); 
-
-            System.out.println("Elemento eliminado (dequeue): " + queueInt.dequeue());
-            System.out.println("Cola después de dequeue: " + queueInt.toString()); 
-
-            System.out.println("Nuevo Front: " + queueInt.front()); 
-            System.out.println("Nuevo Back: " + queueInt.back());
+            System.out.println("atras: " + queueInt.back()); 
 
             System.out.println("Elemento eliminado (dequeue): " + queueInt.dequeue());
+            System.out.println("Cola despues de dequeue: " + queueInt.toString()); 
+
+            System.out.println("Nuevo Frente: " + queueInt.front()); 
+            System.out.println("Nuevo atras: " + queueInt.back());
+
+            System.out.println("Elemento eliminado (dequeue): " + queueInt.dequeue());
             System.out.println("Elemento eliminado (dequeue): " + queueInt.dequeue());
 
-            System.out.println("¿Está vacía? " + queueInt.isEmpty());
+            System.out.println("¿Esta vacia? " + queueInt.isEmpty());
 
         } catch (ExceptionEmpty e) {
             System.out.println("Error detectado: " + e.getMessage());
