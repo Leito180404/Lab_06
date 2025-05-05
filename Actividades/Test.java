1ea661672fb30103fea8e7f90aa2c2f0c027d2f5
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-05-05 07:59:36 -0500

    Update Test.java

--- Actividades/Test.java
+++ Actividades/Test.java
@@ -6,7 +6,24 @@ public class Test {
         stackInt.push(10);
         stackInt.push(20);
         stackInt.push(12);
+        stackInt.push(14);
         System.out.println("Top: " + stackInt.top());
+        System.out.println("Esta llena? " + stackInt.isFull());
+        System.out.println("Contenido de la pila: " + stackInt.toString());
+        System.out.println("Elemento eliminado (pop): " + stackInt.pop());
+        System.out.println("Contenido de la pila despues de pop: " + stackInt.toString());
+
+        System.out.println("Esta llena? " + stackInt.isFull());
+        
+        // Hacemos otro pop
+        System.out.println("Elemento eliminado (pop): " + stackInt.pop()); 
+        System.out.println("Contenido de la pila despues de otro pop: " + stackInt.toString());
+
+        // Hacemos un tercer pop
+        System.out.println("Elemento eliminado (pop): " + stackInt.pop());
+        
+        System.out.println("Esta vacia? " + stackInt.isEmpty());
+
     } catch (ExceptionEmpty e){
         System.out.println("error detectado: " + e.getMessage());
     } 
