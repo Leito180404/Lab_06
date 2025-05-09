2842474b5a5acfe5988e473a2bd542400316b4fd
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-05-09 07:36:11 -0500

    Update Node.java

--- ejercicio1/Node.java
+++ ejercicio1/Node.java
@@ -1,5 +1,35 @@
 package ejercicio1;
 
-public class Node {
-    
+public class Node<T> {
+    private T dato;
+    private Node<T> next;
+
+    public Node(T dato) {
+        this.dato = dato;
+        this.next = null;
+    }
+
+    public T getDato() {
+        return dato;
+    }
+
+    public void setDato(T dato) {
+        this.dato = dato;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "data=" + dato +
+                ", next=" + next +
+                '}';
+    }
 }
