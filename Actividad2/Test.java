1b5a13f17e86757e6d248fab0a4a85d51080f6b9
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-05-08 21:36:04 -0500

    Update Test.java

--- Actividad2/Test.java
+++ Actividad2/Test.java
@@ -2,30 +2,30 @@ package Actividad2;
 
 public class Test {
    public static void main(String[] args) {
-        QueueLink<Integer> queueInt = new QueueLink<>();
+        QueueLink<Integer> cola = new QueueLink<>();
 
-        System.out.println("¿Esta vacia? " + queueInt.isEmpty());
+        System.out.println("¿Esta vacia? " + cola.isEmpty());
 
         try {
-            queueInt.enqueue(10);
-            queueInt.enqueue(20);
-            queueInt.enqueue(30);
-            System.out.println("Cola despues de agregar " + queueInt.toString()); 
+            cola.enqueue(10);
+            cola.enqueue(20);
+            cola.enqueue(30);
+            System.out.println("Cola despues de agregar " + cola.toString()); 
 
-            System.out.println("Frente: " + queueInt.front());
+            System.out.println("Frente: " + cola.front());
 
-            System.out.println("atras: " + queueInt.back()); 
+            System.out.println("atras: " + cola.back()); 
 
-            System.out.println("Elemento eliminado (dequeue): " + queueInt.dequeue());
-            System.out.println("Cola despues de dequeue: " + queueInt.toString()); 
+            System.out.println("Elemento eliminado (dequeue): " + cola.dequeue());
+            System.out.println("Cola despues de dequeue: " + cola.toString()); 
 
-            System.out.println("Nuevo Frente: " + queueInt.front()); 
-            System.out.println("Nuevo atras: " + queueInt.back());
+            System.out.println("Nuevo Frente: " + cola.front()); 
+            System.out.println("Nuevo atras: " + cola.back());
 
-            System.out.println("Elemento eliminado (dequeue): " + queueInt.dequeue());
-            System.out.println("Elemento eliminado (dequeue): " + queueInt.dequeue());
+            System.out.println("Elemento eliminado (dequeue): " + cola.dequeue());
+            System.out.println("Elemento eliminado (dequeue): " + cola.dequeue());
 
-            System.out.println("¿Esta vacia? " + queueInt.isEmpty());
+            System.out.println("¿Esta vacia? " + cola.isEmpty());
 
         } catch (ExceptionEmpty e) {
             System.out.println("Error detectado: " + e.getMessage());
