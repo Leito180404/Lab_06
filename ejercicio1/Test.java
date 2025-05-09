050f4f61c48269fbca8489ad6e43866ee97f6791
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-05-09 08:05:46 -0500

    Update Test.java

--- ejercicio1/Test.java
+++ ejercicio1/Test.java
@@ -1,5 +1,39 @@
 package ejercicio1;
 
+import Actividad1.Stack;
+import Actividad1.ExceptionEmpty;
+
 public class Test {
-    
+    public static void main(String[] args) {
+        Stack<Integer> stackInt = new StackLink<>();
+        Stack<String> stackString = new StackLink<>();
+
+        System.out.println("¿Esta vacia la pila de enteros? " + stackInt.isEmpty());
+        System.out.println("¿Esta vacia la pila de strings " + stackString.isEmpty());
+
+
+        
+        try {
+            //pila de enteros
+            stackInt.push(10);
+            stackInt.push(20);
+            stackInt.push(30);
+            System.out.println("Pila de enteros despues de agregar: ");
+            System.out.println("Top: " + stackInt.top());  // Ver el tope
+            System.out.println("Pop: " + stackInt.pop());  // Eliminar el tope
+            System.out.println("Top despues del pop: " + stackInt.top());
+
+            //pila de cadenas
+            stackString.push("Hola");
+            stackString.push("Mundo");
+            System.out.println("Pila de cadenas despues de agregar: ");
+            System.out.println("Top: " + stackString.top());  // Ver el tope
+            System.out.println("Pop: " + stackString.pop());  // Eliminar el tope
+            System.out.println("Top despues del pop: " + stackString.top());
+
+        } catch (ExceptionEmpty e) {
+            System.out.println("Error detectado: " + e.getMessage());
+        }
+    }
 }
+
