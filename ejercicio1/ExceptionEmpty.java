fae1e0f364c9f3496fb5074b9d75fc03e7ba5691
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-05-09 07:33:08 -0500

    .

---
+++ ejercicio1/ExceptionEmpty.java
@@ -0,0 +1,12 @@
+package ejercicio1;
+
+public class ExceptionEmpty extends Exception {
+
+    public ExceptionEmpty() {
+        super("La pila no tiene nada.");
+    }
+
+    public ExceptionEmpty(String message) {
+        super(message);
+    }
+}
