974df2bf7948b9cc0e280c07845fc3129a7c6226
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-05-09 07:57:04 -0500

    Update StackLink.java

--- ejercicio1/StackLink.java
+++ ejercicio1/StackLink.java
@@ -4,6 +4,40 @@ import Actividad1.ExceptionEmpty;
 import Actividad2.Node;
 
 
-public class StackLink{
-    
+public class StackLink<E> implements Stack<E> {
+    private Node<E> top;
+
+    public StackLink() {
+        this.top = null;
+    }
+
+    @Override
+    public void push(E x) {
+        Node<E> newNode = new Node<>(x); 
+        newNode.setNext(top);
+        top = newNode;
+    }
+
+    @Override
+    public E pop() throws ExceptionEmpty {
+        if (isEmpty()) {
+            throw new ExceptionEmpty("La pila está vacía");
+        }
+        E result = top.getDato();  
+        top = top.getNext();
+        return result;
+    }
+
+    @Override
+    public E top() throws ExceptionEmpty {
+        if (isEmpty()) {
+            throw new ExceptionEmpty("La pila está vacía");
+        }
+        return top.getDato();  
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return top == null;
+    }
 }
