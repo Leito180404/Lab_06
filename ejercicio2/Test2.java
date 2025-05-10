19aa75b244e74abe8ebeaba8ad4b0f63bb0d9235
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-05-09 23:31:54 -0500

    Create Test2.java

---
+++ ejercicio2/Test2.java
@@ -0,0 +1,54 @@
+package ejercicio2;
+
+import Actividad2.Queue;
+import Actividad2.ExceptionEmpty;
+
+public class Test2 {
+    public static void main(String[] args) {
+        Queue<Integer> queueInt = new QueueArray<>(5);
+
+        Queue<String> queueString = new QueueArray<>(5);
+
+        System.out.println("¿Esta vacia la cola de enteros? " + queueInt.isEmpty());
+        System.out.println("¿Esta vacia la cola de strings? " + queueString.isEmpty());
+
+        try {
+            queueInt.enqueue(10);
+            queueInt.enqueue(20);
+            queueInt.enqueue(30);
+            queueInt.enqueue(40);
+            queueInt.enqueue(50);
+
+            try {
+                queueInt.enqueue(60);  // Esto lanzaria una excepcion porque la cola esta llena
+            } catch (IllegalStateException e) {
+                System.out.println("Error al intentar agregar más elementos: " + e.getMessage());
+            }
+
+            System.out.println("Cola de enteros después de agregar: " + queueInt.toString());
+            System.out.println("Frente de la cola de enteros: " + queueInt.front());  // Ver el frente
+            System.out.println("Último de la cola de enteros: " + queueInt.back());  // Ver el ultimo
+            System.out.println("Elemento eliminado (dequeue): " + queueInt.dequeue());  // Eliminar el frente
+            System.out.println("Después de eliminar: " + queueInt.toString());
+            System.out.println("Frente después de eliminar: " + queueInt.front());  // Ver el nuevo frente
+
+            queueString.enqueue("Algoritmos");
+            queueString.enqueue("Estructuras");
+            queueString.enqueue("Datos");
+            queueString.enqueue("Pilas");
+            queueString.enqueue("Colas");
+
+            System.out.println("Cola de cadenas después de agregar: " + queueString.toString());
+            System.out.println("Frente de la cola de cadenas: " + queueString.front());  // Ver el frente
+            System.out.println("Último de la cola de cadenas: " + queueString.back());  // Ver el ultimo
+            System.out.println("Elemento eliminado (dequeue): " + queueString.dequeue());  // Eliminar el frente
+            System.out.println("Después de eliminar: " + queueString.toString());
+            System.out.println("Frente después de eliminar: " + queueString.front());  // Ver el nuevo frente
+
+        } catch (ExceptionEmpty e) {
+            System.out.println("Error detectado: " + e.getMessage());
+        }
+    }
+}
+
+
