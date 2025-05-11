f7444180ab5e2a91bd05ef83f24e8b2297c1df05
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-05-10 19:04:05 -0500

    Create Aplication.java

---
+++ ejercicio4/Aplication.java
@@ -0,0 +1,41 @@
+package ejercicio4;
+
+import Actividad1.ExceptionEmpty;
+import ejercicio1.StackLink;
+
+public class Aplication {
+    public static boolean symbolBalancing(String s) {
+        StackLink<Character> stack = new StackLink<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            }
+            else if (c == ')' || c == ']' || c == '}') {
+                try {
+                    char top = stack.pop();
+                    
+                    if ((c == ')' && top != '(') || (c == ']' && top != '[') || (c == '}' && top != '{')) {
+                        return false;  
+                    }
+                } catch (ExceptionEmpty e) {
+                    return false;  
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        // Pruebas
+        System.out.println(symbolBalancing("()()()[()]"));  // true
+        System.out.println(symbolBalancing("((()))["));    // false
+        System.out.println(symbolBalancing("([)]"));       // false
+        System.out.println(symbolBalancing("[{]}"));       // false
+        System.out.println(symbolBalancing("[[]]{{{}}}")); // true
+    }
+}
+
