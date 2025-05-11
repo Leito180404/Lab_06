ee8f92d163dff93c73c9bd4ac69c8981b5437052
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-05-10 19:19:43 -0500

    Update Test2.java

--- ejercicio2/Test2.java
+++ ejercicio2/Test2.java
@@ -2,29 +2,23 @@ package ejercicio2;
 
 import Actividad2.Queue;
 import Actividad2.ExceptionEmpty;
-
 public class Test2 {
     public static void main(String[] args) {
         Queue<Integer> queueInt = new QueueArray<>(5);
-
         Queue<String> queueString = new QueueArray<>(5);
-
         System.out.println("¿Esta vacia la cola de enteros? " + queueInt.isEmpty());
         System.out.println("¿Esta vacia la cola de strings? " + queueString.isEmpty());
-
         try {
             queueInt.enqueue(10);
             queueInt.enqueue(20);
             queueInt.enqueue(30);
             queueInt.enqueue(40);
             queueInt.enqueue(50);
-
             try {
                 queueInt.enqueue(60);  // Esto lanzaria una excepcion porque la cola esta llena
             } catch (IllegalStateException e) {
                 System.out.println("Error al intentar agregar más elementos: " + e.getMessage());
             }
-
             System.out.println("Cola de enteros despues de agregar: " + queueInt.toString());
             System.out.println("Frente de la cola de enteros: " + queueInt.front());  // Ver el frente
             System.out.println("ultimo de la cola de enteros: " + queueInt.back());  // Ver el ultimo
